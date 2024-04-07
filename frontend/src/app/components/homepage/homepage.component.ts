dbd817add4342e3488fb27de55296037b1ffd219
Serban <serban.crasovan@student.upt.ro>
2024-04-07 19:24:49 +0300

    Add searchbar component

--- frontend/src/app/components/homepage/homepage.component.ts
+++ frontend/src/app/components/homepage/homepage.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { SearchBarComponent } from '../search-bar/search-bar.component';
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [],
+  imports: [SearchBarComponent],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.scss'
 })
