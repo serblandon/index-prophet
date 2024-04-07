b39ac22d3c8acdfc58f28fd673c4447f296f3e14
Serban <serban.crasovan@student.upt.ro>
2024-04-07 19:25:33 +0300

    Create and implement search bar component

---
+++ frontend/src/app/components/search-bar/search-bar.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { TickerService } from '../../services/ticker/ticker.service';
+import { AutoCompleteModule } from 'primeng/autocomplete';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+
+@Component({
+  selector: 'app-search-bar',
+  standalone: true,
+  imports: [AutoCompleteModule, FormsModule, ButtonModule],
+  templateUrl: './search-bar.component.html',
+  styleUrl: './search-bar.component.scss'
+})
+export class SearchBarComponent implements OnInit {
+  ticker = '';
+  suggestions: string[] = [];
+
+  constructor(private router: Router, private tickerService: TickerService) { }
+
+  ngOnInit() {
+    this.tickerService.getTickers().then((tickers: unknown) => {
+      this.suggestions = tickers as string[];
+    }).catch((error: any) => {
+      console.error('Failed to get tickers:', error);
+    });
+  }
+
+
+  searchStock() {
+    if (this.ticker && this.ticker.length > 0) {
+      this.router.navigate(['/stock', this.ticker])
+        .then(() => {
+          console.log('Navigation to stock detail successful!');
+        })
+        .catch(err => {
+          console.error(`Navigation to stock detail failed: ${err}`);
+        });
+    }
+  }
+
+
+  suggest(event: any) {
+    const query = event.query;
+    this.suggestions = Array.from(new Set(
+      this.tickerService.data.filter(ticker => ticker.toLowerCase().startsWith(query.toLowerCase()))
+    ));
+  }
+
+}
