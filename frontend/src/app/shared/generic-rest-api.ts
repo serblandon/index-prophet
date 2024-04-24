a6eecadd97bbe1c8f3dbcb22fd19eb7b32202d24
Serban <serban.crasovan@student.upt.ro>
2024-04-24 23:08:37 +0300

    Add generic rest api class

---
+++ frontend/src/app/shared/generic-rest-api.ts
@@ -0,0 +1,13 @@
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+
+export class GenericRestApi<T> {
+    protected url = '';
+    constructor(protected httpClient: HttpClient, private controller: string = '') {
+        this.url = `/${this.controller}`;
+    }
+  
+    public getByTicker(ticker: string): Observable<T[]> {
+      return this.httpClient.get<T[]>(`${this.url}/${ticker}`);
+    }
+}
