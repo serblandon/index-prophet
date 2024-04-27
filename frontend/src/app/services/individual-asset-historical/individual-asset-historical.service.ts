978a5d926567e18c5fc1af762990e98477931c86
Serban <serban.crasovan@student.upt.ro>
2024-04-27 17:05:30 +0300

    Remove empty line

--- frontend/src/app/services/individual-asset-historical/individual-asset-historical.service.ts
+++ frontend/src/app/services/individual-asset-historical/individual-asset-historical.service.ts
@@ -9,7 +9,6 @@ import { GenericRestApi } from 'src/app/shared/generic-rest-api';
 export class IndividualAssetHistoricalService extends GenericRestApi<IAssetHistoricalData>{
 
   constructor(protected override httpClient: HttpClient) {
-    super(httpClient, "api/HistoricalPrices/GetHistoricalAssetPricesAscending");
+    super(httpClient, "api/HistoricalPrices/GetHistoricalAssetPricesAscendingAsync");
    }
-
 }
