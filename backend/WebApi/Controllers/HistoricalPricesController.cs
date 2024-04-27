6439be5b114e719db5fcfce1c0e89daa9010141d
Serban <serban.crasovan@student.upt.ro>
2024-04-27 16:49:27 +0300

    Add separate controller for Predicted Prices

--- backend/WebApi/Controllers/HistoricalPricesController.cs
+++ backend/WebApi/Controllers/HistoricalPricesController.cs
@@ -2,7 +2,6 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Net;
 using WebApi.Logic.Asset.Queries.GetHistoricalAssetPrices;
-using WebApi.Logic.Asset.Queries.GetPredictedAssetPrices;
 using WebApi.Models;
 
 namespace WebApi.Controllers
@@ -25,22 +24,6 @@ namespace WebApi.Controllers
             ticker = ticker.ToUpper();
 
             var response = await _mediator.Send(new GetHistoricalAssetPricesQuery() { Ticker = ticker} );
-
-            if (!response.Any())
-            {
-                return NotFound();
-            }
-
-            return Ok(response);
-        }
-
-        [HttpGet("[action]/{ticker}/{predictionMethod}")]
-        [ProducesResponseType(typeof(IEnumerable<PredictedPrice>), (int)HttpStatusCode.OK)]
-        public async Task<IActionResult> GetPredictedAssetPricesAscendingAsync(string ticker, string predictionMethod)
-        {
-            ticker = ticker.ToUpper();
-
-            var response = await _mediator.Send(new GetPredictedAssetPricesQuery() { Ticker = ticker, PredictionMethod = predictionMethod });
 
             if (!response.Any())
             {
---
+++ backend/WebApi/Controllers/PredictedPricesController.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using WebApi.Logic.Asset.Queries.GetPredictedAssetPrices;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PredictedPricesController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public PredictedPricesController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("[action]/{ticker}/{predictionMethod}")]
+        [ProducesResponseType(typeof(IEnumerable<PredictedPrice>), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> GetPredictedAssetPricesAscendingAsync(string ticker, string predictionMethod)
+        {
+            ticker = ticker.ToUpper();
+
+            var response = await _mediator.Send(new GetPredictedAssetPricesQuery() { Ticker = ticker, PredictionMethod = predictionMethod });
+
+            if (!response.Any())
+            {
+                return NotFound();
+            }
+
+            return Ok(response);
+        }
+    }
+}
