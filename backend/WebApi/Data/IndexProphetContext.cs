879ab698ad34fdb9dd19d9f7e2d248bb7d837645
Serban <serban.crasovan@student.upt.ro>
2024-04-27 15:28:58 +0300

    Create table and model for predicted prices and modify dbcontext accordingly

--- backend/WebApi/Data/IndexProphetContext.cs
+++ backend/WebApi/Data/IndexProphetContext.cs
@@ -11,6 +11,8 @@ public partial class IndexProphetContext : DbContext
     }
 
     public virtual DbSet<HistoricalPrice> HistoricalPrices { get; set; }
+    public virtual DbSet<PredictedPrice> PredictedPrices { get; set; }
+
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
@@ -22,11 +24,36 @@ public partial class IndexProphetContext : DbContext
 
             entity.HasIndex(e => new { e.Ticker, e.Date }, "historical_prices_ticker_date_key").IsUnique();
 
+            entity.HasIndex(e => e.Ticker, "idx_ticker");
+
             entity.Property(e => e.Id).HasColumnName("id");
             entity.Property(e => e.AdjClosePrice)
                 .HasPrecision(20, 6)
                 .HasColumnName("adj_close_price");
             entity.Property(e => e.Date).HasColumnName("date");
+            entity.Property(e => e.Ticker)
+                .HasMaxLength(12)
+                .HasColumnName("ticker");
+        });
+
+        modelBuilder.Entity<PredictedPrice>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("predicted_prices_pkey");
+
+            entity.ToTable("predicted_prices");
+
+            entity.HasIndex(e => new { e.Ticker, e.PredictionMethod }, "idx_ticker_prediction_method");
+
+            entity.HasIndex(e => new { e.Ticker, e.Date, e.PredictionMethod }, "predicted_prices_ticker_date_key").IsUnique();
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.AdjClosePrice)
+                .HasPrecision(20, 6)
+                .HasColumnName("adj_close_price");
+            entity.Property(e => e.Date).HasColumnName("date");
+            entity.Property(e => e.PredictionMethod)
+                .HasMaxLength(30)
+                .HasColumnName("prediction_method");
             entity.Property(e => e.Ticker)
                 .HasMaxLength(12)
                 .HasColumnName("ticker");
---
+++ backend/WebApi/Models/PredictedPrice.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApi.Models;
+
+public partial class PredictedPrice
+{
+    public int Id { get; set; }
+
+    public string Ticker { get; set; } = null!;
+
+    public DateOnly Date { get; set; }
+
+    public decimal AdjClosePrice { get; set; }
+
+    public string PredictionMethod { get; set; } = null!;
+}
