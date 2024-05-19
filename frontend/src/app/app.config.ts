0c7a84cf4b48f931da21061ff091f1d3bf68dec0
Serban <serban.crasovan@student.upt.ro>
2024-05-19 19:16:47 +0300

    install and setup angular material and dependencies

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -6,7 +6,8 @@ import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideToastr } from 'ngx-toastr';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideHttpClient(withFetch()), provideAnimations(), provideToastr()]
+  providers: [provideRouter(routes), provideClientHydration(), provideHttpClient(withFetch()), provideAnimations(), provideToastr(), provideAnimationsAsync()]
 };
