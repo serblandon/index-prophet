139f1ec9620989918e47d403d5831d43fa75caab
Serban <serban.crasovan@student.upt.ro>
2024-05-25 17:39:09 +0300

    Add 4 major indexes in the home page component

--- frontend/src/app/components/homepage/homepage.component.ts
+++ frontend/src/app/components/homepage/homepage.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SearchBarComponent } from '../search-bar/search-bar.component';
+import { ILivePrice } from 'src/app/models/ILivePrice';
+import { LivePriceService } from 'src/app/services/live-price/live-price.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [SearchBarComponent],
+  imports: [SearchBarComponent, CommonModule],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.scss'
 })
-export class HomepageComponent {
+export class HomepageComponent implements OnInit{
+  livePrices: ILivePrice[] = [];
 
+  constructor(private livePriceService: LivePriceService) {}
+
+  ngOnInit(): void {
+    this.fetchLivePrices();
+  }
+
+  fetchLivePrices() {
+    this.livePriceService.getLivePrices().subscribe(prices => {
+      this.livePrices = prices;
+    });
+  }
 }
