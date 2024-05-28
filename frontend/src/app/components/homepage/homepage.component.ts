5728a272c15f315faf002e823aff61f2cec7255b
Serban <serban.crasovan@student.upt.ro>
2024-05-28 21:04:41 +0300

    Remove live price component

--- frontend/src/app/components/homepage/homepage.component.ts
+++ frontend/src/app/components/homepage/homepage.component.ts
@@ -1,7 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { SearchBarComponent } from '../search-bar/search-bar.component';
-import { ILivePrice } from 'src/app/models/ILivePrice';
-import { LivePriceService } from 'src/app/services/live-price/live-price.service';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -11,18 +9,6 @@ import { CommonModule } from '@angular/common';
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.scss'
 })
-export class HomepageComponent implements OnInit{
-  livePrices: ILivePrice[] = [];
+export class HomepageComponent{
 
-  constructor(private livePriceService: LivePriceService) {}
-
-  ngOnInit(): void {
-    this.fetchLivePrices();
-  }
-
-  fetchLivePrices() {
-    this.livePriceService.getLivePrices().subscribe(prices => {
-      this.livePrices = prices;
-    });
-  }
 }
