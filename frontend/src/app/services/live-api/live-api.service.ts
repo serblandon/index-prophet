63717dd6da3a2d7aa4438df8378d13797a51d8d7
Serban <serban.crasovan@student.upt.ro>
2024-05-29 01:01:42 +0300

    Add company overview api call

--- frontend/src/app/services/live-api/live-api.service.ts
+++ frontend/src/app/services/live-api/live-api.service.ts
@@ -14,7 +14,7 @@ export class LiveApiService{
 
   constructor(protected httpClient: HttpClient, private cacheService: CacheService) {}
 
-  getTopGainersAndLosers(): Observable<any> {
+  public getTopGainersAndLosers(): Observable<any> {
     const cacheKey = 'topGainersAndLosers';
     const cachedData = this.cacheService.get(cacheKey);
     if (cachedData) {
@@ -24,6 +24,19 @@ export class LiveApiService{
     return this.httpClient.get<ILivePrice[]>(`${this.apiUrl}?function=TOP_GAINERS_LOSERS&apikey=${this.apiKey}`).pipe(
       tap(data => this.cacheService.set(cacheKey, data, this.cacheTTL)),
       catchError(this.handleError<any>('getTopGainersAndLosers', []))
+    );
+  }
+
+  public getCompanyOverview(symbol: string): Observable<any> {
+    const cacheKey = `overview-${symbol}`;
+    const cachedData = this.cacheService.get(cacheKey);
+    if (cachedData) {
+      return of(cachedData);
+    }
+
+    return this.httpClient.get<any>(`${this.apiUrl}?function=OVERVIEW&symbol=${symbol}&apikey=${this.apiKey}`).pipe(
+      tap(data => this.cacheService.set(cacheKey, data, this.cacheTTL)),
+      catchError(this.handleError<any>('getCompanyOverview', {}))
     );
   }
 
